68e63fc72c8c0a89319820c9dbe59f868f3d08ea
Taulant Ibraj <tibraj94@gmail.com>
2020-03-28 17:21:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,6 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(array)
+  array.each do |element|
+    puts "Hello, my name is #{element}."
+end
